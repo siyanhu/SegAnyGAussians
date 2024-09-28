458ed7dfb39f92e0ecd5c30e49986889b2d8db5b
siyanhu <siyan.dev@gmail.com>
2024-09-29 01:14:51 +0800

    batch scripts

--- script_pre-train.sh
+++ script_pre-train.sh
@@ -1,24 +1,32 @@
-# python extract_features.py \
-#     --image_root <path to the scene data> \
-#     --sam_checkpoint_path <path to the pre-trained SAM model> \
-#     --downsample <1/2/4/8>
-
-# python extract_segment_everything_masks.py \
-#     --image_root <path to the scene data> \
-#     --sam_checkpoint_path <path to the pre-trained SAM model> \
-#     --downsample <1/2/4/8>
-
-cd data/sub1/images/ && \
-mv seq2/* . && \
-rm -rf seq2 && \
-cd ../../..
-
 python extract_features.py \
-    --image_root data/sub1 \
+    --image_root data/sub3 \
     --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
-    --sam_arch vit_b
-
+    --sam_arch vit_b && \
 python extract_segment_everything_masks.py \
-    --image_root data/sub1 \
+    --image_root data/sub3 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_features.py \
+    --image_root data/sub4 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_segment_everything_masks.py \
+    --image_root data/sub4 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_features.py \
+    --image_root data/sub5 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_segment_everything_masks.py \
+    --image_root data/sub5 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_features.py \
+    --image_root data/sub6 \
+    --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
+    --sam_arch vit_b && \
+python extract_segment_everything_masks.py \
+    --image_root data/sub6 \
     --sam_checkpoint_path third_party/segment-anything/checkpoints/sam_vit_b_01ec64.pth \
     --sam_arch vit_b
--- script_train.sh
+++ script_train.sh
@@ -1,10 +1,40 @@
 python train_scene.py \
-    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub1 \
-    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub1/output_samtgs \
-    --feature_dim 32
-
-
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub2 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub2/output_samtgs \
+    --feature_dim 32 && \
 python train_contrastive_feature.py \
-    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub1 \
-    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub1/output_samtgs \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub2 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub2/output_samtgs \
+    --feature_dim 32 && \
+python train_scene.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub3 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub3/output_samtgs \
+    --feature_dim 32 && \
+python train_contrastive_feature.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub3 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub3/output_samtgs \
+    --feature_dim 32 && \
+python train_scene.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub4 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub4/output_samtgs \
+    --feature_dim 32 && \
+python train_contrastive_feature.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub4 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub4/output_samtgs \
+    --feature_dim 32 && \
+python train_scene.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub5 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub5/output_samtgs \
+    --feature_dim 32 && \
+python train_contrastive_feature.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub5 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub5/output_samtgs \
+    --feature_dim 32 && \
+python train_scene.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub6 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub6/output_samtgs \
+    --feature_dim 32 && \
+python train_contrastive_feature.py \
+    -s /home/siyanhu/Gits/SegAnyGAussians/data/sub6 \
+    -m /home/siyanhu/Gits/SegAnyGAussians/data/sub6/output_samtgs \
     --feature_dim 32
